52227efc546e83694187945229e416c611540206
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-10-22 20:08:14 +0300

    implement cardDAO class

--- lab1/src/db/DAO/CardDAO.java
+++ lab1/src/db/DAO/CardDAO.java
@@ -3,15 +3,33 @@ package db.DAO;
 import db.entities.Card;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by mamax on 10/22/2015.
  */
 public class CardDAO extends GenericDAOImpl<Card> {
+
+    public CardDAO(){
+        tableName = "cards";
+    }
     @Override
     public void insert(Card card) {
-
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            statement.executeQuery(String.format("INSERT INTO cards (doctor_id, user_id, diagnosis_id, name) " +
+                            "VALUES (%s,%s,%s,%s)",
+                    card.getDoctor().getId(),
+                    card.getUser().getId(),
+                    card.getDiagnosis().getId(),
+                    card.getName()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -21,11 +39,51 @@ public class CardDAO extends GenericDAOImpl<Card> {
 
     @Override
     public Card map(ResultSet resultSet) {
-        return null;
+        Card card = null;
+        try {
+            while (resultSet.next()){
+                card = mapCard(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return card;
+    }
+
+    private Card mapCard(ResultSet resultSet)
+    {
+        Card resultCard = new Card();
+
+        try {
+            resultCard.setId(resultSet.getInt("id"));
+            resultCard.setName(resultSet.getString("name"));
+
+            int userId = Integer.parseInt(resultSet.getString("user_id"));
+            int doctorId = Integer.parseInt(resultSet.getString("doctor_id"));
+            int diagnosisId = Integer.parseInt(resultSet.getString("diagnosis_id"));
+
+            resultCard.setUser(DAOFactory.getInstance().getUserDAO().findById(userId));
+            resultCard.setDoctor(DAOFactory.getInstance().getDoctorDAO().findById(doctorId));
+            resultCard.setDiagnosis(DAOFactory.getInstance().getDiagnosisDAO().findById(diagnosisId));
+
+        } catch (SQLException e) {
+            System.out.println("Error while mapping user resultSet");
+            e.printStackTrace();
+        }
+        System.out.println("The result of card getting query is : " + resultCard.toString());
+        return resultCard;
     }
 
     @Override
     public List<Card> mapAll(ResultSet resultSet) {
-        return null;
+        List<Card> cards = new ArrayList<>();
+        try {
+            while (resultSet.next()){
+                cards.add(mapCard(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cards;
     }
 }
