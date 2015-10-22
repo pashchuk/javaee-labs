6d4a55c729218b64bace61f4aea65414f3424f9f
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-10-22 17:12:06 +0300

    move scriptrunner to db package

--- lab1/src/db/ScriptRunner.java
+++ lab1/src/db/ScriptRunner.java
@@ -1,5 +1,5 @@
-/*
- * Slightly modified version of the com.ibatis.common.jdbc.ScriptRunner class
+package db;/*
+ * Slightly modified version of the com.ibatis.common.jdbc.db.ScriptRunner class
  * from the iBATIS Apache project. Only removed dependency on Resource class
  * and a constructor 
  * GPSHansl, 06.08.2015: regex for delimiter, rearrange comment/delimiter detection, remove some ide warnings.
